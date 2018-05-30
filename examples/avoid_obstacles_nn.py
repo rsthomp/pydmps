3594a65274cd9a81264e421c8cc14c1f245f6f18
rachael <rachaelst@mac.com>
2018-05-30 16:15:43 -0400

    postprocessing for - vals

--- examples/avoid_obstacles_nn.py
+++ examples/avoid_obstacles_nn.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras import metrics
+from keras import regularizers
 from sklearn import preprocessing
 import pydmps.dmp_discrete
 
@@ -19,11 +20,11 @@ import pydmps.dmp_discrete
 
 
 beta = 20.0 / np.pi
-gamma = 200
+gamma = 300
 R_halfpi = np.array([[np.cos(np.pi / 2.0), -np.sin(np.pi / 2.0)],
                      [np.sin(np.pi / 2.0), np.cos(np.pi / 2.0)]])
 num_obstacles = 10
-obstacles = [[.5, .02] for i in range(10)]#np.random.random((num_obstacles, 2))* 2 - 1
+obstacles = [[-.5, .02] for i in range(10)]#np.random.random((num_obstacles, 2))* 2 - 1
 
 global c_t
 global obst_vec
@@ -53,7 +54,7 @@ def avoid_obstacles(y, dy, goal, t):
 
         # if we're moving
         if np.linalg.norm(dy) > 1e-5:
-            cur_vel[t] = np.linalg.norm(dy)
+            
             # get the angle we're heading in
             phi_dy = -np.arctan2(dy[1], dy[0])
             obst_dist[t] = dy
@@ -73,6 +74,7 @@ def avoid_obstacles(y, dy, goal, t):
             R = np.dot(R_halfpi, np.outer(obstacle - y, dy))
             pval = -np.nan_to_num(np.dot(R, dy) * dphi)
             c_t[t] = pval
+            cur_vel[t] = np.linalg.norm(obj_vec)
 
             # check to see if the distance to the obstacle is further than
             # the distance to the target, if it is, ignore the obstacle
@@ -91,7 +93,7 @@ tdy_track = np.zeros((dmp.timesteps, dmp.n_dmps))
 tddy_track = np.zeros((dmp.timesteps, dmp.n_dmps))
 
 
-goals = [[1, 0] for i in range(10)]#[[np.cos(theta), np.sin(theta)] for theta in np.linspace(0, 2*np.pi, 20)[:-1]]
+goals = [[-1, 0] for i in range(10)]#[[np.cos(theta), np.sin(theta)] for theta in np.linspace(0, 2*np.pi, 20)[:-1]]
 
 for goal in goals:
     dmp.goal = goal
@@ -100,24 +102,14 @@ for goal in goals:
         ty_track[t], tdy_track[t], tddy_track[t] = dmp.step(external_force=avoid_obstacles(dmp.y, dmp.dy, goal, t))
 
 model = Sequential()
-model.add(Dense(10, activation='relu', input_dim=2))
-#model.add(Dense(10, activation='selu'))
+model.add(Dense(10, activation='relu', bias_regularizer=regularizers.l1(0.01), input_dim=3))
 model.add(Dense(2, activation='tanh'))
 
 
-# print(np.shape(obst_vec[:,0]))
-# print(np.shape(obst_vec[:,1]))
-# print(np.shape(obst_dist))
-# print(np.shape(obst_angle))
-# print(np.shape(cur_vel))
-inpt = np.vstack((obst_vec[:,0], obst_vec[:,1]))#, obst_dist[:,0], obst_dist[:,1], obst_angle))
+
+inpt = np.vstack((obst_vec[:,0], obst_vec[:,1], cur_vel))
 inpt = np.swapaxes(inpt, 0, 1)
-#np.random.shuffle(inpt)
 inpt = preprocessing.scale(inpt)
-
-
-#print(inpt)
-#sys.exit(0)
 
 model.compile(optimizer='adagrad',
               loss='mse', metrics=[metrics.mse])
@@ -126,10 +118,7 @@ label = c_t
 
 
 
-
-model.fit(inpt, label, epochs=20, batch_size=10, verbose=2)
-#print(model.evaluate(inpt, label, batch_size=5, verbose=1))
-#exit(0)
+model.fit(inpt, label, epochs=100, batch_size=10, verbose=2)
 
 y_track = np.zeros((dmp.timesteps, dmp.n_dmps))
 dy_track = np.zeros((dmp.timesteps, dmp.n_dmps))
@@ -138,39 +127,38 @@ ddy_track = np.zeros((dmp.timesteps, dmp.n_dmps))
 """ return a p, the (Cx, Cy) coupling term 
 to avoid an obstacle"""
 def nn_avoid(y, dy, goal, model, postprocess=True):
-	p = np.zeros(2)
-	for obstacle in obstacles:
-		if np.linalg.norm(dy) > 1e-5:
-		        cv = np.linalg.norm(dy)
-		        # get the angle we're heading in
-		        phi_dy = -np.arctan2(dy[1], dy[0])
-		        obstd = dy
+    p = np.zeros(2)
+    for obstacle in obstacles:
+        if np.linalg.norm(dy) > 1e-5:
+                
+                # get the angle we're heading in
+                phi_dy = -np.arctan2(dy[1], dy[0])
+                obstd = dy
 
-		        R_dy = np.array([[np.cos(phi_dy), -np.sin(phi_dy)],[np.sin(phi_dy), np.cos(phi_dy)]])
-		       
-		        # calculate vector to object relative to body
-		        obj_vec = obstacle - y
-		        obstv = obj_vec
+                R_dy = np.array([[np.cos(phi_dy), -np.sin(phi_dy)],[np.sin(phi_dy), np.cos(phi_dy)]])
+               
+                # calculate vector to object relative to body
+                obj_vec = obstacle - y
+                obstv = obj_vec
 
-		        # rotate it by the direction we're going
-		        obj_vec = np.dot(R_dy, obj_vec)
-		        # calculate the angle of obj relative to the direction we're going
-		        phi = np.arctan2(obj_vec[1], obj_vec[0])
-		        obsta = phi
-		        
-		        tst = np.array([[obstv[0]], [obstv[1]]])#, [obstd[0]], [obstd[1]], [obsta], [cv]])
-		        tst = np.swapaxes(tst, 0, 1)
-		        #print(tst)
+                # rotate it by the direction we're going
+                obj_vec = np.dot(R_dy, obj_vec)
+                # calculate the angle of obj relative to the direction we're going
+                phi = np.arctan2(obj_vec[1], obj_vec[0])
+                obsta = phi
+                cv = np.linalg.norm(obj_vec)
+                tst = np.array([[obstv[0]], [obstv[1]], [cv]])#, [obstd[0]], [obstd[1]], [obsta], [cv]])
+                tst = np.swapaxes(tst, 0, 1)
+                #print(tst)
 
-		        pval = model.predict(tst, batch_size=1)
-		        pval = np.array([pval[0][0], pval[0][1]])
-		       
-		        p += pval * gamma
-	#print(p)
+                pval = model.predict(tst, batch_size=1)
+                pval = np.array([pval[0][0], pval[0][1]])
+                
+                p += pval * gamma
         if postprocess:
-	    return postprocess_output(p, y, obstacles[0], goal)
+            return postprocess_output(p, y, obstacles[0], goal)
         return p
- 
+
 
 def get_straight_path(start, goal, timesteps, n_dmps, oscillate = False):
     #constantly make progress to the goal, always move at that angle. 
@@ -182,20 +170,22 @@ def get_straight_path(start, goal, timesteps, n_dmps, oscillate = False):
         dmp_paths[1] = [dmp_paths[1][t] + A*np.sin(30*t) for t in range(len(dmp_paths[0]))]
     path = np.vstack(dmp_paths)
     return path
+
 """ postprocessing steps to ensure safe behaviour"""
 def postprocess_output(x, state, obstacle, goal):
     Cy_original = x[1] #ignore x[0] (the paper told me to do it!!)
     Cy = Cy_original
     #exponentially reduce coupling term to 0 on passing the obstacle
     k = 100
-    if state[0] > obstacle[0]:
+    if abs(state[0]) > abs(obstacle[0]):
         Cy = Cy_original*np.e**(-k*(obstacle[0]-state[0])**2)
     #set coupling term to 0 if obstacle is beyond the goal
-    if state[0] > goal[0]:
+    if abs(state[0]) > abs(goal[0]):
         Cy = 0
     return [0, Cy]
 
-path = get_straight_path([0,0], [1,0.015], dmp.timesteps, dmp.n_dmps, oscillate=True)
+
+path = get_straight_path([0,0], [-1,0.015], dmp.timesteps, dmp.n_dmps, oscillate=False)
 dmp.imitate_path(y_des = path, plot = False)
 
 for goal in goals:
